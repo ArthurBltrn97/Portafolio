41e252d785c2dd71936133a70195c0c83cb5b59b
arturo.beltran <arturo.beltran@ARTUROBELTRAN>
2023-06-22 11:56:12 -0700

    Se agregaron archivos nuevos Agregar archivos de proyecto.

---
+++ Portafolio/Servicios/RepositorioProyectos.cs
@@ -0,0 +1,41 @@
+﻿using Portafolio.Models;
+
+namespace Portafolio.Servicios
+{
+    public interface IRepositorioProyectos
+    {
+        List<Proyecto> ObtenerProyectos();
+    }
+    public class RepositorioProyectos: IRepositorioProyectos
+    {
+        public List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto>()
+            {
+                new Proyecto
+                {
+                    Titulo = "Quesabirrias el Compadre",
+                    Descripcion = "Nada, el otro día compre ahí y me cayó pesado",
+                     Link = "https://zelda.fandom.com/es/wiki/Link",
+                    ImageURL = "/imagenes/Quesadilla.jpg "
+                },
+
+                new Proyecto
+                {
+                    Titulo = "Tacos Valle Alto",
+                    Descripcion = "Ahí estaban buenos los tacos pero mi tío traspaso la taqueria",
+                    Link = "https://zelda.fandom.com/es/wiki/Link",
+                    ImageURL = "/imagenes/tacos.png "
+                },
+
+                new Proyecto
+                {
+                    Titulo = "DAKI Sushi",
+                    Descripcion = "Buen ambiente laboral, me hicieron chivo jajaj",
+                    Link = "https://zelda.fandom.com/es/wiki/Link",
+                    ImageURL = "/imagenes/sushi.jpg "
+                }
+            };
+        }
+    }
+}
